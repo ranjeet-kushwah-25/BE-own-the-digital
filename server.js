673388b59884aef43d40c1a08205f8968b46ded1
Ranjeet Kushwah <ranjitkush25@gmail.com>
2026-04-29 15:21:41 +0530

    add domain names

--- server.js
+++ server.js
@@ -31,8 +31,8 @@ app.use(helmet());
 // CORS configuration
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? ['https://be-own-the-digital.onrender.com/api-docs'] // Add your frontend domain here
-    : ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:5173','https://be-own-the-digital.onrender.com/api-docs'],
+    ? ['https://be-own-the-digital.onrender.com/api-docs', 'https://own-the-digital-8c2b.vercel.app'] // Add your frontend domain here
+    : ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:5173','https://be-own-the-digital.onrender.com/api-docs','https://own-the-digital-8c2b.vercel.app'],
   credentials: true
 }));
 
