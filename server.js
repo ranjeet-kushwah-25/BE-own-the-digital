f0de4ce080205daeb0c211f820e0f0facceedbfd
Ranjeet Kushwah <ranjitkush25@gmail.com>
2026-05-01 13:59:21 +0530

    remove console.log on server

--- server.js
+++ server.js
@@ -144,7 +144,6 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
-  console.log(`API Documentation: http://localhost:${PORT}/api-docs`);
 });
 
 // Handle unhandled promise rejections
